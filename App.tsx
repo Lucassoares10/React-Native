48abb720feb90ed643fa061eca7dd54a3d80ac2a
Lucas Da Silva Soares (Basis) <lucassoares@mec.gov.br>
2021-11-14 13:17:16 -0300

    Realizado implementação e instalação das fontes do app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'; 
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani'; 
+import AppLoading from 'expo-app-loading'; 
 import { SignIn } from './src/screens/Signin';
 
 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  })
+
+  /*Equanto as fontes nao estiverem totalmente carregadas, segurar na parte inicial */
+  if(!fontsLoaded){
+   return <AppLoading />
+  }
+
   return (
     <SignIn />
   );
