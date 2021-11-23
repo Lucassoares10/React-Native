3f2cf840b7b2b37890d0f373d66f273ed020818c
Lucassoares10 <ls6359575@gmail.com>
2021-11-23 10:42:30 -0300

    Realizado ajustes na autenticação do usuario dentro do App

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,48 +1,50 @@
-
 import React from 'react';
-import { View, Text, Image, StatusBar } from 'react-native';
-import { ButtonIcon } from '../../components/ButtonIcon';
-import { Background } from "../../components/Background";
+import {View, Text, Image, StatusBar} from 'react-native';
+import {ButtonIcon} from '../../components/ButtonIcon';
+import {Background} from "../../components/Background";
 import IllustrationImg from "../../assets/illustration.png";
-import { styles } from './styles';
-import { useNavigation } from '@react-navigation/native';
+import {styles} from './styles';
+import {useNavigation} from '@react-navigation/native';
+import {useAuth} from "../../hooks/auth";
 
 
 export function SignIn() {
 
     const navigation = useNavigation();
+    const {user} = useAuth();
+
 
     function handleSignIn() {
-      navigation.navigate('Home');
+        navigation.navigate('Home');
 
     }
 
     /* const[text, setText] = useState(''); |||||||Criar um estado(text=nome), (função que atualiza o estado (Set.nome)) */
 
-  return (
-      <Background>
-          <View style={styles.container}>
+    return (
+        <Background>
+            <View style={styles.container}>
 
-              <Image source={IllustrationImg} style={styles.image} resizeMode="stretch"/>
+                <Image source={IllustrationImg} style={styles.image} resizeMode="stretch"/>
 
-              <View style={styles.content}>
-                  <Text style={styles.title}>
-                      Conecte-se {`\n`}
-                      e organize suas {`\n`}
-                      jogatinas
-                  </Text>
+                <View style={styles.content}>
+                    <Text style={styles.title}>
+                        Conecte-se {`\n`}
+                        e organize suas {`\n`}
+                        jogatinas
+                    </Text>
 
-                  <Text style={styles.subtitle}>
-                      Crie grupos para jogar seus games{`\n`}
-                      favoritos com seus amigos
-                  </Text>
+                    <Text style={styles.subtitle}>
+                        Crie grupos para jogar seus games{`\n`}
+                        favoritos com seus amigos
+                    </Text>
 
-                  {/*Caso queira reutilizar componente trocar o title para mudança */}
-                  <ButtonIcon title="Entrar com Discord"
-                              onPress={handleSignIn}
-                  />
-              </View>
-          </View>
-      </Background>
-  );
+                    {/*Caso queira reutilizar componente trocar o title para mudança */}
+                    <ButtonIcon title="Entrar com Discord"
+                                onPress={handleSignIn}
+                    />
+                </View>
+            </View>
+        </Background>
+    );
 }
